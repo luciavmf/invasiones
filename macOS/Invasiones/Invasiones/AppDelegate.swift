0e247af02b9d24c5c300023836cc96c378e8e8bb
luciavmf <luciavmf@gmail.com>
2026-04-03 15:30:06 +0200

    Add initial macOS project

---
+++ macOS/Invasiones/Invasiones/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Invasiones
+//
+//  Created by Lucia Medina Fretes on 03.04.26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ macOS/Invasiones/InvasionesTests/InvasionesTests.swift
@@ -0,0 +1,19 @@
+//
+//  InvasionesTests.swift
+//  InvasionesTests
+//
+//  Created by Lucia Medina Fretes on 03.04.26.
+//
+
+import Testing
+@testable import Invasiones
+
+struct InvasionesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
