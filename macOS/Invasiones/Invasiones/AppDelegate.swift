f81e13ddea76fd04c963d7405130f42902a31cb0
luciavmf <luciavmf@gmail.com>
2026-04-09 16:48:10 +0200

    Delete Main.storyboard
    Build the main view programatically

--- macOS/Invasiones/Invasiones/AppDelegate.swift
+++ macOS/Invasiones/Invasiones/AppDelegate.swift
@@ -5,20 +5,30 @@
 //  Created by Lucia Medina Fretes on 03.04.26.
 //
 
-
 import Cocoa
 
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+
+    private var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let viewController = ViewController()
+
+        let contentRect = NSRect(x: 0, y: 0, width: ScreenSize.width, height: ScreenSize.height)
+        window = NSWindow(
+            contentRect: contentRect,
+            styleMask:   [.titled, .closable, .miniaturizable, .resizable],
+            backing:     .buffered,
+            defer:       false
+        )
+        window.title = "Invasiones"
+        window.contentViewController = viewController
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
     }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
-    
-    
 }
